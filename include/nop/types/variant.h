72e5777f22b2c4ff07aa0270f1c7e11f10477781
Finlay Marno <finlay.marno@codeplay.com>
2025-04-23 17:22:05 +0100

    Add missing template args list after the use of the template keyword
    Since https://github.com/llvm/llvm-project/pull/80801, clang requires an
    explicit template list after the use of the template keyword for
    template member function.

--- include/nop/types/variant.h
+++ include/nop/types/variant.h
@@ -238,7 +238,7 @@ class Variant {
   // resulting type.
   template <typename... Args>
   void Construct(Args&&... args) {
-    index_ = value_.template Construct(std::forward<Args>(args)...);
+    index_ = value_.template Construct<>(std::forward<Args>(args)...);
   }
   void Construct(EmptyVariant) {}
 
@@ -255,14 +255,14 @@ class Variant {
   // multiple element types.
   template <typename T, typename U>
   void Assign(TypeTag<T>, U&& value) {
-    if (!value_.template Assign(TypeTag<T>{}, index_, std::forward<U>(value))) {
+    if (!value_.template Assign<>(TypeTag<T>{}, index_, std::forward<U>(value))) {
       Destruct();
       Construct(TypeTag<T>{}, std::forward<U>(value));
     }
   }
   template <typename T>
   void Assign(T&& value) {
-    if (!value_.template Assign(index_, std::forward<T>(value))) {
+    if (!value_.template Assign<>(index_, std::forward<T>(value))) {
       Destruct();
       Construct(std::forward<T>(value));
     }
